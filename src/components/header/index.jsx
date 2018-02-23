ea8045c9a95ff7409074f96386710b10c1f7271c
陆溢昕 <410780496@qq.com>
2018-02-24 00:02:01 +0800

    增加内容

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,53 +3,28 @@
 */
 
 import React, { Component } from 'react'
-
+import { hearderIcons } from '@/static/data/icon'
 import './header.less'
 
-const icons = [
-  {
-    id: 0,
-    icon: 'icon-yinyue'
-  },
-  {
-    id: 1,
-    icon: 'icon-changpian'
-  },
-  {
-    id: 2,
-    icon: 'icon-duorenyonghu'
-  }
-]
-
 class Header extends Component {
-  constructor () {
-    super()
-    this.state = {
-      activeItemId: 1
-    }
+  constructor (props) {
+    super(props)
   }
 
   attachClassName (id, className) {
-    return ('iconfont ' + className + (id === this.state.activeItemId ? ' active' : ''))
-  }
-
-  switchActiveMod (id) {
-    if (id === this.state.activeItemId) return
-    this.setState({
-      activeItemId: id
-    })
+    let diff = ''
+    diff = !id ? ' left' : id === 4 ? ' right' : id === this.props.activeHeaderId ? ' active' : ''
+    return (`iconfont ${className}${diff}`)
   }
 
   render () {
-    const iconItem = icons.map((a, i) => (
-      <i className={this.attachClassName(a.id, a.icon)} key={i} aria-hidden="true" onClick={this.switchActiveMod.bind(this, a.id)}></i>
+    const iconItem = hearderIcons.map((a, i) => (
+      <i className={this.attachClassName(a.id, a.icon)} key={i} aria-hidden="true" onClick={this.props.iconClick.bind(null, a.id)}></i>
     ))
     // console.log(iconItem)
     return (
       <div className="component-header">
-        <i className="iconfont icon-hanbaocaidan left" aria-hidden="true"></i>
         { iconItem }
-        <i className="iconfont icon-xingzhuang right" aria-hidden="true"></i>
       </div>
     )
   }
--- src/views/main.jsx
+++ src/views/main.jsx
@@ -1,15 +1,40 @@
+/*
+* created by lu.yixin on 2018/02/22
+*/
+
 import React, { Component } from 'react'
 import Header from '@/components/header'
+import LeftMenu from '@/components/leftMenu'
 
 class Main extends Component {
   constructor () {
     super()
+    this.state = {
+      activeHeaderId: 2,
+      showLeftMenu: false
+    }
+  }
+  iconClick (id) {
+    if (id === this.state.activeHeaderId) return
+    if (!id) {
+      return this.setState({
+        showLeftMenu: true
+      })
+    }
+    if (id === 4) return
+    this.setState({
+      activeHeaderId: id
+    })
   }
 
+  test () {
+    console.log(1)
+  }
   render () {
     return (
-      <div>
-        <Header />
+      <div onClick={this.test}>
+        <Header activeHeaderId={this.state.activeHeaderId} iconClick={this.iconClick.bind(this)}/>
+        <LeftMenu showLeftMenu={this.state.showLeftMenu}/>
         main
       </div>
     )
